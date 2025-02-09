d16b287caccf037b257e07a31d4c36b040b7aa7a
ryanngu-afk <rngu82@gmail.com>
2025-02-09 08:35:59 -0800

    learned about HSV
    added hsv filter for real time filter updates

--- main.py
+++ main.py
@@ -8,13 +8,21 @@ from vision import Vision
 #WindowCapture.list_window_names()
 wincap = WindowCapture('Mabinogi')
 vision_test = Vision('image.png')
+vision_test.init_control_gui()
 
 while(True):
     screenshot = wincap.get_screenshot()
 
+    #object detection
     rectangles = vision_test.find(screenshot)
+    points = vision_test.get_click_points(rectangles)
 
-    cv.imshow('debug',vision_test.draw_rectangles(screenshot,rectangles))
+    #draw on image
+    #output_image = vision_test.draw_click_points(screenshot,points)
+    output_image = vision_test.apply_hsv_filter(screenshot)
+
+    #display processed image
+    cv.imshow('debug',output_image)
 
     if cv.waitKey(1) == ord('q'):
         cv.destroyAllWindows()
